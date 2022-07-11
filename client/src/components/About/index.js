b60a54d5d6a7b13e445ab774f64faaba44007de1
stevey d <stphndcky22@gmail.com>
2022-07-10 18:19:04 -0700

    added profile pic to about section

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import me from "../../assets/stephen-dickey.jpg";
+
 const About = () => {
   return (
     <div className="info">
         <h2>About Me</h2>
+        <img className="info-img" src={me} alt="welcome"/>
         <p>I have been living in Oakland for the last 6 years and was born and raised in the Bay Area. I have a background working in EMS and healthcare and recently gained an interest in Software Development. I am currently completing a Coding Bootcamp through UC Berkeley Extension and hope to gain employment in the field following course completion.</p>
     </div>
   )
--- client/src/components/Projects/index.js
+++ client/src/components/Projects/index.js
@@ -4,6 +4,9 @@ import React from "react";
 import aimlessTravel from "../../assets/aimless-travel.png";
 import noteTaker from "../../assets/note-taker.png";
 import techBlog from "../../assets/tech-blog.png";
+import pizzaHunt from "../../assets/pizza-hunt.png";
+import yummyGrocery from "../../assets/yummy-grocery.png";
+import deepThoughts from "../../assets/deep-thoughts.png";
 
 const Projects = () => {
   return (
@@ -20,6 +23,18 @@ const Projects = () => {
         <div className="projects-container">
             <h3>Aimless Travel</h3>
             <a href="https://blooming-escarpment-07246.herokuapp.com/"><img src={aimlessTravel} alt="destination-quiz Project"/></a>
+        </div>
+        <div className="projects-container">
+            <h3>Pizza Hunt</h3>
+            <a href="https://immense-castle-41602.herokuapp.com/"><img src={pizzaHunt} alt="note-taker Project"/></a>
+        </div> 
+        <div className="projects-container">
+            <h3>Yummy Grocery</h3>
+            <a href="https://secure-journey-79025.herokuapp.com/"><img src={yummyGrocery} alt="tech-blog Project"/></a>
+        </div> 
+        <div className="projects-container">
+            <h3>Deep Thoughts</h3>
+            <a href="https://murmuring-brook-04465.herokuapp.com/"><img src={deepThoughts} alt="destination-quiz Project"/></a>
         </div> 
    
     </div>
